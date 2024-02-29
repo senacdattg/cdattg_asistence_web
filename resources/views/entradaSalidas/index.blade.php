237a1616510f7df6f89e56468c802193b8b4021f
Williamlo21 <lopezrebellonw@gmail.com>
2024-02-29 18:04:21 -0500

    se comienza la implementacion del index entrada salida

--- resources/views/entradaSalidas/index.blade.php
+++ resources/views/entradaSalidas/index.blade.php
@@ -21,9 +21,93 @@
         <section class="content">
 
             <div class="card">
-               
+
                 <div class="card-body">
-                    Aqui va el registro de aprendices
+                    <div class="card-body p-0">
+                        <table class="table table-responsive">
+                            <thead>
+                                <tr>
+                                    <th style="width: 1%">
+                                        #
+                                    </th>
+                                    <th style="width: 20%">
+                                        Nombre y apellido
+                                    </th>
+                                    <th style="width: 30%">
+                                        Numero de documento
+                                    </th>
+                                    <th style="width: 40%">
+                                        Correo electronico
+                                    </th>
+                                    <th style="width: 50%">
+                                        Estado
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($personas as $persona)
+                                    <tr>
+                                        <td>
+                                            {{ $persona->id }}
+                                        </td>
+                                        <td>
+                                            {{ $persona->primer_nombre }} {{ $persona->primer_apellido }}
+                                        </td>
+
+                                        <td>
+                                            {{ $persona->numero_documento }}
+                                        </td>
+                                        <td>
+                                            {{ $persona->email }}
+                                        </td>
+                                        <td>
+                                            {{-- <span class="badge badge-{{ $persona->user->status === 1 ? 'success' : 'danger' }}">
+                                        @if ($persona->user->status === 1)
+                                                ACTIVO
+                                            @else
+                                                INACTIVO
+                                            @endif
+                                        </span> --}}
+                                        </td>
+                                        <td>
+                                            {{-- <form id="cambiarEstadoForm" class=" d-inline"
+                                            action="{{ route('persona.cambiarEstadoUser', ['persona' => $persona->user->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('PUT') --}}
+                                            <button type="submit" class="btn btn-success btn-sm"><i
+                                                    class="fas fa-sync"></i></button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            {{-- <a class="btn btn-warning btn-sm"
+                                            href="{{ route('persona.show', ['persona' => $persona->id]) }}"> --}}
+                                            <i class="fas fa-eye"></i>
+
+                                            </a>
+                                        </td>
+                                        <td>
+                                            {{-- <a class="btn btn-info btn-sm"
+                                            href="{{ route('persona.edit', ['persona' => $persona->id]) }}"> --}}
+                                            <i class="fas fa-pencil-alt">
+                                            </i>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            {{-- <form action="{{ route('personas.destroy', ['persona' => $persona->id]) }}"
+                                            method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE') --}}
+
+                                            {{-- <button type="submit" class="btn btn-danger btn-sm"
+                                            onclick="return confirm('¿Estás seguro de que deseas eliminar este parámetro?')">
+
+                                            <i class="fas fa-trash"></i>
+                                        </button> --}}
+                                            {{-- </form> --}}
+                                        </td>
+
+                    </div>
 
                 </div>
             @endsection
