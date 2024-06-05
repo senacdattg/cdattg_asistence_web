5a9dc3f5070ee62bf1af8f7e7d83d05ac45a9b9a
Williamlo21 <lopezrebellonw@gmail.com>
2024-06-05 18:29:21 -0500

    ajuste bloques-vista

--- resources/views/sede/index.blade.php
+++ resources/views/sede/index.blade.php
@@ -6,7 +6,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1>{{ request()->path() }}
+                        <h1>Sedes
 
 
                         </h1>
@@ -14,9 +14,9 @@
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item">
-                                {{-- <a href="{{ route('home.index') }}">Inicio</a> --}}
+                                <a href="{{ route('home.index') }}">Inicio</a>
                             </li>
-                            <li class="breadcrumb-item active">{{ request()->path() }}
+                            <li class="breadcrumb-item active">Sedes
                             </li>
                         </ol>
                     </div>
