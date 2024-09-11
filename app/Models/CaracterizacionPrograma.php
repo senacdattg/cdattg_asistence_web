d6efba3167998c0a67519feafae1f18000917e61
Alarcon-CoordinacionAcademica <academicacoordinacionguaviare@gmail.com>
2024-09-11 17:31:10 -0500

    se añadieron modelos

--- app/Models/CaracterizacionPrograma.php
+++ app/Models/CaracterizacionPrograma.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class FichaCaracterizacion extends Model
+class CaracterizacionPrograma extends Model
 {
     use HasFactory;
 }
