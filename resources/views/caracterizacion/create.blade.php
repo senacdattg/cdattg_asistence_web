1bd780ed7a3c930915490186fd52e644a9107bba
Alarcon-CoordinacionAcademica <academicacoordinacionguaviare@gmail.com>
2024-10-09 11:27:50 -0500

    inicio de actualización para crear caracterizaciones

--- resources/views/caracterizacion/create.blade.php
+++ resources/views/caracterizacion/create.blade.php
@@ -27,83 +27,16 @@
                                     @endif
                                     </select>
                                 </div>
-                            </div>
+                            </div> 
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label for="programa_formacion_id">Programa de formación</label>
-                                    <select name="programa_formacion_id" class="form-control select2" id="programa_formacion_id" required>
-                                        @if(count($programas) > 0)
-                                            @foreach($programas as $programa)
-                                                <option value="{{ $programa->id }}">{{ $programa->nombre }}</option>
-                                            @endforeach
-                                        @else
-                                            <option value="">No hay programas disponibles</option>
-                                        @endif
-                                    </select>
+                                    <button type="submit" class="btn btn-primary" style="margin-top: 32px;">Seleccionar</button>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="instructor_persona_id">Instructor</label>
-                                    <select name="instructor_persona_id" class="form-control select2" id="instructor_persona_id" required>
-                                        @if(count($instructores) > 0)
-                                            @foreach($instructores as $instructor)
-                                                <option value="{{ $instructor->persona_id }}">{{ $instructor->persona->primer_nombre }}</option>
-                                            @endforeach
-                                        @else
-                                            <option value="">No hay instructores disponibles</option>
-                                        @endif
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="jornada_id">Jornada de formación</label>
-                                    <select name="jornada_id" class="form-control select2" id="jornada_id" required>
-                                        @if(count($jornadas) > 0)
-                                            @foreach($jornadas as $jornada)
-                                                <option value="{{ $jornada->id }}">{{ $jornada->jornada }}</option>
-                                            @endforeach
-                                        @else
-                                            <option value="">No hay jornadas disponibles</option>
-                                        @endif
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="sede_id">Sede</label>
-                                    <select name="sede_id" class="form-control select2" id="sede_id" required>
-                                        @if(count($sedes) > 0)
-                                            @foreach($sedes as $sede)
-                                                <option value="{{ $sede->id }}">{{ $sede->sede }}</option>
-                                            @endforeach
-                                        @else
-                                            <option value="">No hay sedes disponibles</option>
-                                        @endif
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Guardar</button>
                     </form>
                 </div>
             </div>
         </div>
     </section>
 </div>
-
-<!-- Inicializar Select2 -->
-<script>
-    $(document).ready(function() {
-        $('.select2').select2({
-            placeholder: "Seleccione una opción",
-            allowClear: true
-        });
-    });
-</script>
 @endsection
