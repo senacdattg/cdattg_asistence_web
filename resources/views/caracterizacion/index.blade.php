b64f0ac27e45f874e608dd5e39a0259d4697e00b
Alarcon Steven <academicacoordinacionguaviare@gmail.com>
2024-11-28 16:36:59 -0500

    cambio en las alertas

--- resources/views/caracterizacion/index.blade.php
+++ resources/views/caracterizacion/index.blade.php
@@ -75,11 +75,7 @@
                                 <td class="text-center">{{$caracter->ficha->ficha}}</td>
                                 <td class="text-center">{{$caracter->programaFormacion->nombre}}</td>
                                 <td class="text-center">{{$caracter->persona->primer_nombre }} {{$caracter->persona->primer_apellido ?? ''}}</td>
-<<<<<<< HEAD
-                                <td class="text-center">{{$caracter->jornada->Jornada}}</td>
-=======
                                 <td class="text-center">{{$caracter->jornada->jornada ?? 'N/A' }}</td>
->>>>>>> feature/jornada_crud
                                 <td class="text-center">{{$caracter->sede->sede}}</td>
                                 
                             
