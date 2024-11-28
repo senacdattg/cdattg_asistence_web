5420f1c68af74e826865f2162673a2d90176712e
Alarcon Steven <academicacoordinacionguaviare@gmail.com>
2024-11-28 10:54:41 -0500

    se implementó el controlador nuevamente

---
+++ app/Http/Controllers/JornadaController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JornadaController extends Controller
+{
+    public function index()
+    {
+        return view('jornada.index');
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function show($id)
+    {
+        
+    }
+
+    public function edit($id)
+    {
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        
+    }
+}
--- resources/views/jornada/index.blade.php
+++ resources/views/jornada/index.blade.php
@@ -1,4 +1,7 @@
 @extends('layout.master-layout')
 @section('content')
+
 <h1>AQUI SE MOSTRARAN LAS JORNADAS</h1>
+
 @endsection
+
