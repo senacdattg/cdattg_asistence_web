b57e5b2e9e0cb8bddacfb33109ecff043a57c612
Alarcon Steven <academicacoordinacionguaviare@gmail.com>
2024-12-06 09:21:46 -0500

    errror tipografico

--- app/Models/parametro.php
+++ app/Models/parametro.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class parametro extends Model
+class Parametro extends Model
 {
     use HasFactory;
 
